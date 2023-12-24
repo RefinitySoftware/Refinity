2f50d4f4e61a3924fd719ef0d644904be279492d
Francesco Cogliandro <gastoncello@live.it>
2023-12-24 14:30:18 +0100

    Delete MLUtility.cs, MLModels.cs, and custom.cs files

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System.Globalization;
-using Refinity.ML;
-using Refinity.ML.Models;
 using Refinity.Math;
 
 public static partial class Program
@@ -9,14 +7,7 @@ public static partial class Program
     {
         try
         {
-            string[] features = {"Years_of_Experience","Age","Education_Level","Location","Salary"};
-            string labelColumnName = "Salary";
-            string featureColumnName = "Features";
-            string outputColumnName = "Salary";
-            string inputColumnName = "Features";
-            string scoreColumnName = "Score";
-            MLModels mLModels = new MLModels(features, labelColumnName, featureColumnName, outputColumnName, inputColumnName, scoreColumnName);
-            MLUtility.LinearRegressionModel<CustomModel>(@"C:\Users\gasto\Desktop\Progetti\Refinity\Testing\data\salary_regression_dataset.csv", mLModels);
+            
         }
         catch (Exception ex)
         {
