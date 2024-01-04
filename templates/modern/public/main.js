da7e1d57fe80995d0d8e8bc24e9e5176b28d9cf2
Francesco <stellar.database@gmail.com>
2024-01-04 11:12:02 +0100

    Add logo styling to main.css and update main.js with icon links

--- templates/modern/public/main.js
+++ templates/modern/public/main.js
@@ -1,1 +1,10 @@
-export default {}
+export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+      {
+        icon: 'github',
+        href: 'https://github.com/InfinitySoftware-House/Refinity',
+        title: 'GitHub'
+      },
+    ]
+}
