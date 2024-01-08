8c06bec7c2911dbb4fd09d23a8f3a38768dd8f57
Francesco <stellar.database@gmail.com>
2024-01-08 17:59:53 +0100

    Add descriptions to EnumMonths

--- Refinity/Enums/EnumsUtility.cs
+++ Refinity/Enums/EnumsUtility.cs
@@ -65,28 +65,64 @@ public static class EnumsUtility
 /// </summary>
 public enum EnumMonths
 {
+    /// <summary>
+    /// Represents the month of January.
+    /// </summary>
     [Description("January")]
     January,
+    /// <summary>
+    /// Represents the month of February.
+    /// </summary>
     [Description("February")]
     February,
+    /// <summary>
+    /// Represents the month of March.
+    /// </summary>
     [Description("March")]
     March,
+    /// <summary>
+    /// Represents the month of April.
+    /// </summary>
     [Description("April")]
     April,
+    /// <summary>
+    /// Represents the month of May.
+    /// </summary>
     [Description("May")]
     May,
+    /// <summary>
+    /// Represents the month of June.
+    /// </summary>
     [Description("June")]
     June,
+    /// <summary>
+    /// Represents the month of July.
+    /// </summary>
     [Description("July")]
     July,
+    /// <summary>
+    /// Represents the month of August.
+    /// </summary>
     [Description("August")]
     August,
+    /// <summary>
+    /// Represents the month of September.
+    /// </summary>
     [Description("September")]
     September,
+    /// <summary>
+    /// Represents the month of October.
+    /// </summary>
     [Description("October")]
     October,
+    /// <summary>
+    /// Represents the month of November.
+    /// </summary>
     [Description("November")]
     November,
+    /// <summary>
+    /// Represents the month of December.
+    /// </summary>
     [Description("December")]
     December
 }
