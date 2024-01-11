8fb3145bfa26d7f34438d000eacbd85fb53b5241
Francesco <stellar.database@gmail.com>
2024-01-11 17:55:56 +0100

    Update project structure and remove unused code

--- TestingConsole/Program.cs
+++
@@ -1,3 +0,0 @@
-using Refinity.IO;
-
-IOUtility.CompressFile("/Users/francesco/Desktop/Screenshot 2023-12-30 alle 15.26.33.png", "/Users/francesco/Desktop/Screenshot 2023-12-30 alle 15.26.33.png.gz");
